07fa2f1516b8544bdf7ca0f935618fce0383cb32
KaiWhen <wangkw523@gmail.com>
2018-09-30 17:42:49 +0100

    rps yes

--- rps.js
+++ rps.js
@@ -1,9 +1,14 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
 
 var rps;
 var rpsrnd;
 var rpsact = false;
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} lsjkdkfsdbf`);
+});
 
 bot.on("message", async message => {
   if(message.author.bot) return;
@@ -18,52 +23,55 @@ bot.on("message", async message => {
         message.channel.send("Type rock, paper, or scissors");
         rpsact = true;
         rpsrnd = Math.floor(Math.random()*2)
-        if(msg.content === 'rock' && rpsrnd === 0 && rpsact === true){
+      }
+        if(message.content === 'rock' && rpsrnd === 0 && rpsact === true){
            message.channel.send("rock");
            message.channel.send("It's a draw!");
            rpsact = false;
         }
-        else if(msg.content === 'rock' && rpsrnd === 1 && rpsact === true){
+        else if(message.content === 'rock' && rpsrnd === 1 && rpsact === true){
           message.channel.send("paper");
           message.channel.send("HA! I win. What a loserrr");
           rpsact = false;
         }
-        else if(msg.content === 'rock' && rpsrnd === 2 && rpsact === true){
+        else if(message.content === 'rock' && rpsrnd === 2 && rpsact === true){
           message.channel.send("scissors");
           message.channel.send("SHIT FUCK");
           rpsact = false;
         }
-        else if(msg.content === 'paper' && rpsrnd === 0 && rpsact === true){
+        else if(message.content === 'paper' && rpsrnd === 0 && rpsact === true){
           message.channel.send("rock");
           message.channel.send("SHIT FUCK");
           rpsact = false;
         }
-        else if(msg.content === 'paper' && rpsrnd === 1 && rpsact === true){
+        else if(message.content === 'paper' && rpsrnd === 1 && rpsact === true){
           message.channel.send("paper");
           message.channel.send("It's a draw!");
           rpsact = false;
         }
-        else if(msg.content === 'paper' && rpsrnd === 2 && rpsact === true){
+        else if(message.content === 'paper' && rpsrnd === 2 && rpsact === true){
           message.channel.send("scissors");
           message.channel.send("HA! I win. What a loserrr");
           rpsact = false;
         }
-        else if(msg.content === 'scissors' && rpsrnd === 0 && rpsact === true){
+        else if(message.content === 'scissors' && rpsrnd === 0 && rpsact === true){
           message.channel.send("rock");
           message.channel.send("HA! I win. What a loserrr");
           rpsact = false;
         }
-        else if(msg.content === 'scissors' && rpsrnd === 1 && rpsact === true){
+        else if(message.content === 'scissors' && rpsrnd === 1 && rpsact === true){
           message.channel.send("paper");
           message.channel.send("SHIT FUCK");
           rpsact = false;
         }
-        else if(msg.content === 'scissors' && rpsrnd === 2 && rpsact === true){
+        else if(message.content === 'scissors' && rpsrnd === 2 && rpsact === true){
           message.channel.send("scissors");
           message.channel.send("It's a draw!");
           rpsact = false;
         }
         return;
-    }
+
 
 });
+
+bot.login(process.env.BOT_TOKEN);
