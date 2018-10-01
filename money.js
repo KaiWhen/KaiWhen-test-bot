b79664151282ddf7193a49cc6cb347ffd85fd8b6
KaiWhen <wangkw523@gmail.com>
2018-10-01 23:20:41 +0100

    jhgf

--- money.js
+++ money.js
@@ -102,7 +102,7 @@ bot.on("message", async message => {
     }
 
     if (!itemName) {
-        message.channel.send('**Item not found.**');
+      return message.channel.send('**Item not found.**');
     }
 
         if (userData[message.author.id + message.guild.id].money <= itemPrice) {
@@ -123,7 +123,7 @@ bot.on("message", async message => {
             }
 
 
-
+return;
 
 
 }
