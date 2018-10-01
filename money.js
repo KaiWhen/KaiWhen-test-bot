2b4489c515e5ed8dffc96ca7cfe4d6aa7adf96c0
KaiWhen <wangkw523@gmail.com>
2018-10-01 02:16:11 +0100

    lkjhg

--- money.js
+++ money.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 const fs = require ("fs");
 let userData = JSON.parse(fs.readFileSync('./userData.json', 'utf8'));
+const moment = require ("moment");
 
 bot.on("ready", async () => {
   console.log('money.js is running');
@@ -20,6 +21,7 @@ bot.on("message", async message => {
 
   if(!userData[message.author.id + message.guild.id]) userData[message.author.id + message.guild.id] = {}
   if(!userData[message.author.id + message.guild.id].money) userData[message.author.id + message.guild.id].money = 500;
+  if(!userData[message.author.id + message.guild.id].prevDaily) userData[message.author.id + message.guild.id].prevDaily = "Not Collected";
 
   fs.writeFile("./userData.json", JSON.stringify(userData), (err) => {
     if (err) console.error(err);
@@ -27,8 +29,23 @@ bot.on("message", async message => {
 
   if(cmd === `${prefix}money`){
     let moneyEmbed = new Discord.RichEmbed()
-    .addField("Balance", userData[message.author.id + message.guild.id].money)
+    .addField("Balance", userData[message.author.id + message.guild.id].money + " kaiwhency")
      message.channel.send(moneyEmbed);
+
+     if(cmd === `${prefix}daily`){
+       if(userData[message.author.id + message.guild.id].prevDaily != moment().format('L')){
+         userData[message.author.id + message.guild.id].prevDaily = moment().format('L')
+         userData[message.author.id + message.guild.id].money += 200;
+
+         let dailyEmbed = new Discord.RichEmbed()
+         .addField("Daily Reward", "You have just received 200 kaiwhency!")
+          message.channel.send(dailyEmbed);
+
+       }
+       else{
+         message.channel.send("don't be greedy ffs");
+       }
+
 
   }
 
