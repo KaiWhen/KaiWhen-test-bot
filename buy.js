d0c79eb0387a25dbc999b456ddf7ab9475b9c796
KaiWhen <wangkw523@gmail.com>
2018-10-01 22:11:38 +0100

    fsadfadfsad

--- buy.js
+++ buy.js
@@ -23,86 +23,11 @@ bot.on("message", async message => {
   let msg = message.content.toUpperCase();
 
     
-    if(msg.startsWith(`${prefix}BUY`)){
-
-        let categories = [];
-
-
-
-        if(!args.join(" ")){
-
-            for(var i in items){
-
-                if(!categories.includes(items[i].type)){
-                    categories.push(items[i].type)
-
-                }
-            }
-                const itemEmbed = new Discord.RichEmbed()
-                .setDescription("Items")
-
-                for(i = 0; i < categories.length; i++){
-
-                    var iDesc = '';
-
-                    for(var c in items){
-
-                        if(categories[i] === items[c].type){
-
-                            iDesc += `${items[c].name} | ${items[c].price} | ${items[c].desc}\n`;
-                        }
-                    }
-
-                    itemEmbed.addField(categories[i], iDesc);
-                    
-                }
-                
-                message.channel.send({itemEmbed});
-
-        }
-
-        let itemName = '';
-        let itemPrice = 0;
-        let itemDesc = '';
-
-        for (var i in items) { 
-            if (args.join(" ").trim().toUpperCase() === items[i].name.toUpperCase()) { 
-                itemName = items[i].name;
-                itemPrice = items[i].price;
-                itemDesc = items[i].desc;
-            }
-        }
-
-        if (itemName === '') {
-            return message.channel.send(`**Item ${args.join(" ").trim()} not found.**`);
-        }
-
-            if (userData[message.author.id + message.guild.id].money <= itemPrice) { 
-
-                return message.channel.send(`**You don't have enough money for this item.**`);
-            }
-
-            if (userData[message.author.id + message.guild.id].money >= itemPrice) { 
-
-                message.channel.send('**You bought ' + itemName + '!**');
-                userData[message.author.id + message.guild.id].money -= itemPrice;
-
-            }
-
-                
-                if (itemName === 'Test Command') {
-                    message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "dont mind me")); 
-                }
-
-            
-
-        
-
-    }
+    
 
 
 
 
 });
 
-bot.login(process.env.BOT_TOKEN);
+
--- money.js
+++ money.js
@@ -17,7 +17,7 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
-
+  let msg = message.content.toUpperCase();
 
   if(!userData[message.author.id + message.guild.id]) userData[message.author.id + message.guild.id] = {}
   if(!userData[message.author.id + message.guild.id].money) userData[message.author.id + message.guild.id].money = 500;
@@ -49,6 +49,83 @@ bot.on("message", async message => {
 
 
   }
+
+  if(msg.startsWith(`${prefix}BUY`)){
+
+    let categories = [];
+
+
+
+    if(!args.join(" ")){
+
+        for(var i in items){
+
+            if(!categories.includes(items[i].type)){
+                categories.push(items[i].type)
+
+            }
+        }
+            const itemEmbed = new Discord.RichEmbed()
+            .setDescription("Items")
+
+            for(i = 0; i < categories.length; i++){
+
+                var iDesc = '';
+
+                for(var c in items){
+
+                    if(categories[i] === items[c].type){
+
+                        iDesc += `${items[c].name} | ${items[c].price} | ${items[c].desc}\n`;
+                    }
+                }
+
+                itemEmbed.addField(categories[i], iDesc);
+                
+            }
+            
+            message.channel.send({itemEmbed});
+
+    }
+
+    let itemName = '';
+    let itemPrice = 0;
+    let itemDesc = '';
+
+    for (var i in items) { 
+        if (args.join(" ").trim().toUpperCase() === items[i].name.toUpperCase()) { 
+            itemName = items[i].name;
+            itemPrice = items[i].price;
+            itemDesc = items[i].desc;
+        }
+    }
+
+    if (itemName === '') {
+        return message.channel.send(`**Item ${args.join(" ").trim()} not found.**`);
+    }
+
+        if (userData[message.author.id + message.guild.id].money <= itemPrice) { 
+
+            return message.channel.send(`**You don't have enough money for this item.**`);
+        }
+
+        if (userData[message.author.id + message.guild.id].money >= itemPrice) { 
+
+            message.channel.send('**You bought ' + itemName + '!**');
+            userData[message.author.id + message.guild.id].money -= itemPrice;
+
+        }
+
+            
+            if (itemName === 'Test Command') {
+                message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "dont mind me")); 
+            }
+
+        
+
+    
+
+}
 
 
 
